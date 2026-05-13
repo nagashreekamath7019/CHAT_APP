4a5d636051e028b267793d4467ca8edcf7d51202
Nagashree <nagashreekamath7048@gmail.com>
2026-05-13 16:26:54 +0530

    user

--- backend/index.js
+++ backend/index.js
@@ -15,8 +15,7 @@ dotenv.config();
 app.use(cors({
     origin: "https://chatternode.onrender.com",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type","Authorization"]
+    methods: ["GET", "POST", "PUT", "DELETE"]
 }));
 
 app.use(express.json());
